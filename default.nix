123cc80e40f1f7a0349e46065052f20302425006
firefly-cpp <iztok@iztok-jr-fister.eu>
2023-02-04 17:49:28 +0100

    Format code

--- default.nix
+++ default.nix
@@ -1,9 +1,8 @@
-{
-  pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/e2f0148a2052cb6d948d7f5326cde18391f48f5f.tar.gz";
-    sha256 = "sha256:11rnjp2bhp1sds5kdbyprlq09blhgr5ayzibmsmc4g2lmr27vvk2";
-  }) {}
-}:
+{ pkgs ? import (fetchTarball {
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/e2f0148a2052cb6d948d7f5326cde18391f48f5f.tar.gz";
+  sha256 = "sha256:11rnjp2bhp1sds5kdbyprlq09blhgr5ayzibmsmc4g2lmr27vvk2";
+}) { } }:
 
 with pkgs;
 
@@ -27,31 +26,26 @@ let
         (python310.buildEnv.override {
           ignoreCollisions = true;
           extraLibs = with python310Packages; [
-          # Add Python packages
-          niapy
-          niaaml
-          niaarm
-        ];
-      })
+            # Add Python packages
+            niapy
+            niaaml
+            niaarm
+          ];
+        })
       ];
     };
 
     # shell of our environment
     experiment = mkShell rec {
       name = "experiment1";
-      buildInputs = [
-        python310Packages.niapy
-      ];
-      shellHook = "
-      cd res*/bin
-      ./run.sh";
+      buildInputs = [ python310Packages.niapy ];
+      shellHook = "\n      cd res*/bin\n      ./run.sh";
     };
 
     meta = {
-        description = "Simple experiment using niapy package";
-        homepage = "https://gitlab.com/firefly-cpp/nia-test-experiments";
-      };
+      description = "Simple experiment using niapy package";
+      homepage = "https://gitlab.com/firefly-cpp/nia-test-experiments";
+    };
 
   };
-in
-  packages
+in packages
